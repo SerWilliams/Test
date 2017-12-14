b01c5a4a43b69b869d867abf157f32a332e819cd
SerWilliams <32600200+SerWilliams@users.noreply.github.com>
2017-12-14 14:01:40 +0300

    Update export_fl.py

--- export_fl.py
+++ export_fl.py
@@ -1,5 +1,6 @@
 import pyodbc
-import logging
+from pika import PlainCredentials, BlockingConnection, ConnectionParameters, BasicProperties
+from decimal import Decimal
 import logging.config
 from datetime import datetime, timedelta
 from json import dumps, dump
@@ -8,69 +9,74 @@ from zlib import compress
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen, HTTPError, URLError
 from socket import gethostname
-logging.config.fileConfig('log_conf.cfg')
+from os import makedirs
+jsdir = 'C:\\scot\\export\\FLreports'
+makedirs(jsdir, exist_ok=True)
+logging.config.fileConfig('..\\log_conf.cfg')
 logger = logging.getLogger("export_fl")
 logger.info('='*60)
 logger.info('Запуск экспорта данных из БД FLReports.mdb')
 Config = {'id_oo': 0, 'code': gethostname()[1:7]}
-dt = datetime.strftime(datetime.now() - timedelta(days=20), "%Y-%m-%d")
+dt = datetime.strftime(datetime.now() - timedelta(days=7), "%Y-%m-%d")
 logger.info('ИД ГМ - %d, код ГМ - %s, дата начала %s', Config['id_oo'], Config['code'], dt)
 try:
-    conn = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb)};DBQ=D:\\FLReports.mdb')
+    conn = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb)};DBQ=C:\\scot\\Report\\Database\\FLReports.mdb')
     logger.debug('Успешное подключение к БД')
 except pyodbc.Error as e:
     logger.error('Ошибка БД \n%s \nExit', e)
     exit()
 
 
-def sql_query(query, table, rpath, inti, stri, *floati):
-    logger.info('Выгрузка таблицы %s', table)
+def sql_query(*args):
+    logger.info('Выгрузка таблицы %s', args[1])
     cursor = conn.cursor()
-    cursor.execute(query)
+    cursor.execute(args[0])
     columns = [i[0] for i in cursor.description]
     rows = []
     for stroka in cursor.fetchall():
-        for item in inti:
+        for item in args[3]:
             if stroka[item]:
                 stroka[item] = int(stroka[item])
-        for item in stri:
+        for item in args[4]:
             if stroka[item]:
                 stroka[item] = str(stroka[item])
-        for item in floati:
-            if stroka[item]:
-                stroka[item] = float(stroka[item].replace(',','.'))
-        rows.append(dict(zip(columns, stroka)))
-    result = {table: {'columns': columns, 'rows': rows}}
-    with open(rpath, 'w') as file:
-        logger.info('Запись в файл %s', rpath)
-        dump(result, file, indent=4, sort_keys=True, ensure_ascii=False)
-    send_result({
-        'id_oo': Config['id_oo'],
-        'code': Config['code'],
-        'method': 'gm_kso_flreports',
-        'data': compress(dumps(result, sort_keys=True, ensure_ascii=False).encode('utf-8'))
-    })
-
-
-def send_result(data):
-    url_in = "http://10.5.4.61:875/api/in_data.php"
-    url_in = 'https://httpbin.org'
-    logger.info('Отправка данных на сервер %s', url_in)
-    send_data = urlencode(data).encode('utf-8')
-    r = Request(url_in, send_data)
-    sendstatus = 1
-    while sendstatus <=3:
         try:
-            urlopen(r)
-            logger.info('Отправка выполнена успешно')
-        except HTTPError as e:
-            logger.error('Ошибка подключения к серверу %s, попытка %d ~HTTPError~ %s', e.code, sendstatus, e.reason)
-            sendstatus += 1
-        except URLError as e:
-            logger.error('Ошибка подключения к серверу, попытка %d ~URLError~ %s', sendstatus, e.reason)
-            sendstatus += 1
-        else:
-            sendstatus = 4
+            for item in args[5]:
+                if stroka[item]:
+                    stroka[item] = float(stroka[item].replace(',','.'))
+        except:
+            pass
+        rows.append(dict(zip(columns, stroka)))
+    result = {args[1]: {'columns': columns, 'rows': rows}}
+    with open(args[2], 'w') as file:
+        logger.info('Запись в файл %s', args[2])
+        dump(result, file, indent=4, sort_keys=True, ensure_ascii=False)
+    try:
+     send_messege(compress(dumps(result, sort_keys=True, ensure_ascii=False).encode('utf-8')))
+    except Exception as e:
+        logger.error('Ошибка отправки данных на сервер %s', e)
+
+
+def send_messege(message):
+    logger.info('Отправка данных на сервер')
+    secure = PlainCredentials('sadmin', 'akuma')
+    connect = BlockingConnection(
+        ConnectionParameters(
+         host='10.5.4.15',
+         port=5672,
+         virtual_host='/',
+         credentials=secure
+        )
+    )
+    channel = connect.channel()
+    channel.basic_publish(
+        exchange='gm_kso_report',
+        routing_key='',
+        body=message,
+        properties=BasicProperties(
+         delivery_mode=2,
+        )
+    )
 
 
 query_accept = ('''
@@ -113,17 +119,21 @@ StationID, TransactionID, TenderType, TenderTotal, TenderOperatorID, TenderStart
 from Tenders
 where TransactionID > '%s' order by  TransactionID desc;
 ''' % dt)
-try:
-    sql_query(query_accept, 'CurrencyAccepted', 'D:\\FLreports\\json\\accept.json', (0,2,), (3,))
-    sql_query(query_dispens, 'CurrencyDispensed', 'D:\\FLreports\\json\\dispens.json', (0,2,), (3,))
-    sql_query(query_manager, 'CurrencyCashManagement', 'D:\\FLreports\\json\\manager.json', (0,3,5,), (7,))
-    sql_query(query_deverr, 'DeviceErrors', 'D:\\FLreports\\json\\deverr.json', (0,), (3,))
-    sql_query(query_appmode, 'ApplicationMode', 'D:\\FLreports\\json\\appmode.json', (0,), (6,))
-    sql_query(query_interven, 'Interventions', 'D:\\FLreports\\json\\interven.json', (0,7,), (4,5,))
-    sql_query(query_itemex, 'ItemExceptions', 'D:\\FLreports\\json\\itemex.json', (0,), (7,), 4, 5)
-    sql_query(query_tenders, 'Tenders', 'D:\\FLreports\\json\\tenders.json', (0,3,4,), (5,6,))
-except BaseException as e:
-    logger.error('Ошибка выполнения запроса \n%s \n', e)
+queryes = [
+    [query_accept, 'CurrencyAccepted', jsdir + '\\accept.json', (0,2,), (3,)],
+    [query_dispens, 'CurrencyDispensed', jsdir + '\\dispens.json', (0,2,), (3,)],
+    [query_manager, 'CurrencyCashManagement', jsdir + '\\manager.json', (0,3,5,), (7,)],
+    [query_deverr, 'DeviceErrors', jsdir + '\\deverr.json', (0,), (3,)],
+    [query_appmode, 'ApplicationMode', jsdir + '\\appmode.json', (0,), (6,)],
+    [query_interven, 'Interventions', jsdir + '\\interven.json', (0,7,), (4,5,)],
+    [query_itemex, 'ItemExceptions', jsdir + '\\itemex.json', (0,), (7,), (4, 5,)],
+    [query_tenders, 'Tenders', jsdir + '\\tenders.json', (0,3,4,), (5,6,)]
+]
+for i in range(len(queryes)):
+    try:
+        sql_query(*queryes[i])
+    except BaseException as e:
+        logger.error('Ошибка выполнения запроса \n%s \n', e)
 logger.info('Выгрузка завершена.')
 logging.shutdown()
 exit(1)
